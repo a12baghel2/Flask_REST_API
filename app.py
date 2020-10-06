e94aae2bb60b594548c825df4b39aa5ee2028e10
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2020-10-06 18:00:51 +0530

    Updated Styling and Index.html

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
 @app.route('/search/')
 def search():
     entity_type = request.args.get('key','')
